c14085d579b714746d16e2aa29e9dd18c4d2b701
weijiazhen <289154842@qq.com>
2019-08-06 11:16:09 +0800

    8/6提交

---
+++ src/components/Tabbar/Home.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="home">
+    <!-- 1.搜索 -->
+    <van-search
+      style="padding-left:45px;"
+      background="#fff034"
+      class="fixedSearch"
+      v-model="value"
+      placeholder=" 潮流新品特卖 "
+      show-action
+      @click="toSearch()"
+      shape="round"
+    >
+      <div slot="action">
+        <img
+          src="http://w.wantscart.com/static/images/3.png"
+          width="22"
+          style="position:absolute;left:10px;top:20px;"
+        />
+      </div>
+      <div slot="action">
+        <img
+          src="http://w.wantscart.com/static/images/2.png"
+          width="22"
+          style="margin-top:10px"
+          @click="newsTo"
+        />
+      </div>
+    </van-search>
+    <!-- 2.标签栏 -->
+    <van-tabs animated line-width="75px" line-height="1px" :swipe-threshold="navNum">
+      <!-- 推荐 -->
+      <van-tab :title="navArr[0]">
+        <Tuijian />
+      </van-tab>
+      <!-- 居家 -->
+      <van-tab :title="navArr[1]">
+        <Jujia />
+      </van-tab>
+      <!-- 服装 -->
+      <van-tab :title="navArr[2]">
+        <Fuzhuang />
+      </van-tab>
+      <!-- 鞋靴 -->
+      <van-tab :title="navArr[3]">
+        <Shoes />
+      </van-tab>
+      <!-- 包类 -->
+      <van-tab :title="navArr[4]">
+        <Bag />
+      </van-tab>
+    </van-tabs>
+    <!-- 尾部 -->
+    <PubFooter />
+  </div>
+</template>
+<script>
+// 推荐
+import Tuijian from "../Home/Tuijian";
+import Fuzhuang from "../Home/Fuzhuang";
+// 鞋靴
+import Shoes from "../Home/Shoes";
+// 包类
+import Bag from "../Home/Bag";
+// 公共底部
+import PubFooter from "../Home/PubFooter";
+import Jujia from "../Home/Jujia";
+export default {
+  data() {
+    return {
+      value: "", //1.搜索框
+      navArr: ["推荐", "居家", "服装", "鞋靴", "包类"],
+      navNum: 5
+    };
+  },
+  methods: {
+    navTo(name) {
+      // 编程式导航
+      this.$router.push({
+        name
+      });
+    },
+    newsTo() {
+      this.$router.push("/news");
+    },
+    toSearch(){
+       this.$router.push({
+          name: "search"
+       })
+    }
+  },
+  components: {
+    Tuijian,
+    PubFooter,
+    Shoes,
+    Bag,
+    Jujia,
+    Fuzhuang
+  }
+};
+</script>
+
+
+
+
+
+
---
+++ src/vants/index.js
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+// import vant from 'vant'
+// 第三方组件
+// 搜索
+import {
+    //头部搜索框
+    Search,
+    //底部导航
+    Tabbar,
+    TabbarItem,
+    //图标引入
+    Icon,
+    //导航栏
+    NavBar,
+    //输入框
+    Field,
+    // 按钮
+    Button,
+    // 单元格
+    Cell,
+    CellGroup,
+    //Tab
+    Tab,
+    Tabs,
+    //轮播图
+    Swipe,
+    SwipeItem,
+    // image
+    Image,
+    // 宫格
+    Grid,
+    GridItem,
+    //商品导航
+    GoodsAction,
+    GoodsActionIcon,
+    GoodsActionButton
+} from 'vant';
+
+
+export default () => {
+    Vue.use(Tabbar);
+    Vue.use(TabbarItem);
+    Vue.use(Search);
+    Vue.use(Icon);
+    //导航栏
+    Vue.use(NavBar);
+    // 输入框
+    Vue.use(Field);
+    // 按钮
+    Vue.use(Button);
+    // 单元格
+    Vue.use(Cell).use(CellGroup);
+    // tab
+    Vue.use(Tab).use(Tabs);
+    // 轮播图
+    Vue.use(Swipe).use(SwipeItem);
+    // Image 
+    Vue.use(Image);
+    // 宫格
+    Vue.use(Grid).use(GridItem);
+    //商品导航
+    Vue.use(GoodsAction).use(GoodsActionIcon).use(GoodsActionButton);
+}
